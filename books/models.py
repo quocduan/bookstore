dcec33af87aacfcf0d737495d86f1afa31ac2b6b
Homoske <duan@cjs.vn>
2018-01-31 22:04:43 +0700

    rename *_id --> *_id1 before add foreignkeys

--- books/models.py
+++ books/models.py
@@ -11,7 +11,7 @@ from django.db import models
 
 
 class Authors(models.Model):
-    author_id = models.CharField(max_length=4)
+    author_id1 = models.CharField(max_length=4)
     name = models.CharField(max_length=25, blank=True, null=True)
     url = models.CharField(max_length=80, blank=True, null=True)
 
@@ -24,7 +24,7 @@ class Books(models.Model):
     # International Standard Book Number (ISBN) is a unique numeric commercial book identifier.
     isbn = models.CharField(max_length=13)
     title = models.CharField(max_length=60, blank=True, null=True)
-    publisher_id = models.CharField(max_length=5, blank=True)
+    publisher_id1 = models.CharField(max_length=5, blank=True)
     url = models.CharField(max_length=80, blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
@@ -35,7 +35,7 @@ class Books(models.Model):
 
 class BooksAuthors(models.Model):
     isbn = models.CharField(max_length=13)
-    author_id = models.CharField(max_length=4)
+    author_id1 = models.CharField(max_length=4)
     # https://en.wikipedia.org/wiki/Book_series
     seq_no = models.SmallIntegerField(blank=True, null=True)
 
@@ -45,7 +45,7 @@ class BooksAuthors(models.Model):
 
 
 class Publishers(models.Model):
-    publisher_id = models.CharField(max_length=5)
+    publisher_id1 = models.CharField(max_length=5)
     name = models.CharField(max_length=30, blank=True, null=True)
     url = models.CharField(max_length=80, blank=True, null=True)
 
