f39839f75dfd615fc7eea47f2611e9faff5600bd
Homoske <duan@cjs.vn>
2018-01-31 15:50:48 +0700

    makemigrations-fake

--- books/models.py
+++ books/models.py
@@ -11,7 +11,7 @@ from django.db import models
 
 
 class Authors(models.Model):
-    author_id = models.CharField(max_length=4, primary_key=True)
+    author_id = models.CharField(max_length=4)
     name = models.CharField(max_length=25, blank=True, null=True)
     url = models.CharField(max_length=80, blank=True, null=True)
 
@@ -22,9 +22,9 @@ class Authors(models.Model):
 
 class Books(models.Model):
     # International Standard Book Number (ISBN) is a unique numeric commercial book identifier.
-    isbn = models.CharField(max_length=13, primary_key=True)
+    isbn = models.CharField(max_length=13)
     title = models.CharField(max_length=60, blank=True, null=True)
-    publisher_id = models.CharField(max_length=5, blank=True, null=False)
+    publisher_id = models.CharField(max_length=5, blank=True, null=True)
     url = models.CharField(max_length=80, blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
@@ -45,7 +45,7 @@ class Booksauthors(models.Model):
 
 
 class Publishers(models.Model):
-    publisher_id = models.CharField(max_length=5, primary_key=True)
+    publisher_id = models.CharField(max_length=5,blank=True, null=True)
     name = models.CharField(max_length=30, blank=True, null=True)
     url = models.CharField(max_length=80, blank=True, null=True)
 
