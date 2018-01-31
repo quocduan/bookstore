b9a5b8156a1264b578a130c5b5a569365a299b98
Homoske <duan@cjs.vn>
2018-01-31 15:48:34 +0700

    makemigrations

--- books/models.py
+++ books/models.py
@@ -11,42 +11,44 @@ from django.db import models
 
 
 class Authors(models.Model):
-    author_id = models.CharField(max_length=4)
+    author_id = models.CharField(max_length=4, primary_key=True)
     name = models.CharField(max_length=25, blank=True, null=True)
     url = models.CharField(max_length=80, blank=True, null=True)
 
     class Meta:
-        managed = False
+        # managed = False
         db_table = 'authors'
 
 
 class Books(models.Model):
+    # International Standard Book Number (ISBN) is a unique numeric commercial book identifier.
+    isbn = models.CharField(max_length=13, primary_key=True)
     title = models.CharField(max_length=60, blank=True, null=True)
-    isbn = models.CharField(max_length=13)
-    publisher_id = models.CharField(max_length=5, blank=True, null=True)
+    publisher_id = models.CharField(max_length=5, blank=True, null=False)
     url = models.CharField(max_length=80, blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
     class Meta:
-        managed = False
+        # managed = False
         db_table = 'books'
 
 
 class Booksauthors(models.Model):
     isbn = models.CharField(max_length=13)
     author_id = models.CharField(max_length=4)
+    # https://en.wikipedia.org/wiki/Book_series
     seq_no = models.SmallIntegerField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        # managed = False
         db_table = 'booksauthors'
 
 
 class Publishers(models.Model):
-    publisher_id = models.CharField(max_length=5, blank=True, null=True)
+    publisher_id = models.CharField(max_length=5, primary_key=True)
     name = models.CharField(max_length=30, blank=True, null=True)
     url = models.CharField(max_length=80, blank=True, null=True)
 
     class Meta:
-        managed = False
+        # managed = False
         db_table = 'publishers'
