bbc9b74603fa1877a4b04807d8f8449736e9a274
Homoske <duan@cjs.vn>
2018-02-01 17:58:03 +0700

    add Blog models

--- othermodels/models.py
+++ othermodels/models.py
@@ -10,3 +10,34 @@ class Item(models.Model):
 
     class Meta:
         ordering = ["name"]
+
+
+# BLOG models
+# https://docs.djangoproject.com/en/2.0/topics/db/queries/
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+    n_comments = models.IntegerField()
+    n_pingbacks = models.IntegerField()
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return self.headline
